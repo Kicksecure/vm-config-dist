7424a2c5c31c2e9dd6d1232e7ef3cd5ffb9e6dae
Patrick Schleizer <adrelanos@riseup.net>
2015-06-17 19:22:08 +0000

    fix 'su -' issue, refactoring - https://phabricator.whonix.org/T358

--- etc/profile.d/20_power_savings_disable_in_vms.sh
+++ etc/profile.d/20_power_savings_disable_in_vms.sh
@@ -4,54 +4,43 @@
 ## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
 ## See the file COPYING for copying conditions.
 
-## Using `return` in combination with `exit` so this script can be both,
-## being `source`d as well as executed.
+output_cmd="true"
+#output_cmd="echo"
 
-virt_what_command_exit_code="0"
-command -v virt-what >/dev/null || { virt_what_command_exit_code="$?" ; true; };
+script_name="power-savings-disable-in-vms"
 
-if [ "$virt_what_command_exit_code" = "0" ]; then
-   true "$0: virt-what found. Continue."
-elif [ -x "/usr/sbin/virt-what" ]; then
-   true "$0: /usr/sbin/virt-what executable. Continue."
-else
-   true "$0: virt-what not found. Stop."
+true $script_name: Using "return" in combination with "exit" so this script can be both, being "source"d as well as executed.
+
+if ! shopt -q login_shell ; then
+   $output_cmd "$script_name: Not running in a login shell, not doing anything."
+   return
+   exit 0
+fi
+
+if ! test -x /usr/sbin/virt-what >/dev/null ; then
+   $output_cmd "$script_name: /usr/sbin/virt-what not executable found. Stop."
    return 0
    exit 0
 fi
 
-setterm_command_exit_code="0"
-command -v setterm >/dev/null || { setterm_command_exit_code="$?" ; true; };
-
-if [ ! "$setterm_command_exit_code" = "0" ]; then
-   true "$0: setterm not installed. Stop."
+if ! command -v setterm >/dev/null ; then
+   $output_cmd "$script_name: setterm not installed. Stop."
    return 0
    exit 0
-else
-   true "$0: setterm found. Continue."
 fi
 
 result="$(sudo virt-what)"
 
 if [ "$result" = "" ]; then
-   true "Not running in a Virtual Machine (or none detected), therefore not disabling monitor power saving. Stop."
+   $output_cmd "$script_name:Not running in a Virtual Machine (or none detected), therefore not disabling monitor power saving. Stop."
    return 0
    exit 0
-else
-   true "$0: VM $result found. Continue."
 fi
 
-if [ -d "/usr/lib/qubes" ]; then
-   setterm -blank 0 2>/dev/null
-   true "$0: exit code: $?"
-   setterm -powerdown 0 2>/dev/null
-   true "$0: exit code: $?"
-else
-   setterm -blank 0
-   true "$0: exit code: $?"
-   setterm -powerdown 0
-   true "$0: exit code: $?"
-fi
+setterm -blank 0 2>/dev/null
+$output_cmd "$script_name: exit code: $?"
+setterm -powerdown 0 2>/dev/null
+$output_cmd "$script_name: exit code: $?"
 
 return 0
 exit 0
